690032581545e841dbd8d1daf3b0417fbccab2f2
Ian Macalinao <ian@giza.us>
2014-09-06 14:18:59 -0500

    Integrate apple chart

--- chrome/ext.js
+++ chrome/ext.js
@@ -29,6 +29,26 @@ function loadBloomberg() {
                    + '<hr />'
                    + '<small>Powered by Bloomberg</small>'
       });
+
+    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+        // Create the chart
+        $('#chart-aapl').highcharts('StockChart', {
+            rangeSelector : {
+                selected : 1,
+                inputEnabled: $('#container').width() > 480
+            },
+            title : {
+                text : 'AAPL Stock Price'
+            },
+            series : [{
+                name : 'AAPL',
+                data : data,
+                tooltip: {
+                    valueDecimals: 2
+                }
+            }]
+        });
+    });
     });
   });
 }
