da68bc98da26ffbb2ff593e9e43c5ee09b2d69ea
Ian Macalinao <ian@giza.us>
2014-09-06 17:38:54 -0500

    Improve performance

--- chrome/ext.js
+++ chrome/ext.js
@@ -5,33 +5,33 @@
 
 var URL = "http://cywoods.wynd07.com/work/";
 
-function api(url, data, callb){
-    $.ajax({
-        type: 'GET', 
-            url: url,
-            async: true,
-            cache: false,
-            dataType: 'json',
-            data: (data!=""&&data!=null ? data : ""), 
-            success: function(data, textStatus, jqXHR){
-              if (typeof data === "undefined" || data == null){
-                callb(null);
-                alert("Request failed");
-                return;
-              }
-              callb(data);
-            }
-        });
+function api(url, data, callb) {
+  $.ajax({
+    type: 'GET',
+    url: url,
+    async: true,
+    cache: false,
+    dataType: 'json',
+    data: (data != "" && data != null ? data : ""),
+    success: function(data, textStatus, jqXHR) {
+      if (typeof data === "undefined" || data == null) {
+        callb(null);
+        alert("Request failed");
+        return;
+      }
+      callb(data);
+    }
+  });
 }
 
 var stocks = [];
 
-$(document).ready(function(){
+$(document).ready(function() {
   //api tests
-  api(URL + "AAPL.json", null, function(data){
+  api(URL + "AAPL.json", null, function(data) {
     console.log(data);
   });
-  api(URL+"list.json", null, function(data){
+  api(URL + "list.json", null, function(data) {
     console.log(data);
     stocks = data;
     loadBloomberg();
@@ -47,6 +47,7 @@ $(document).ready(function(){
 function loadBloomberg() {
   var myStocks = {};
   $(function() {
+    var names = [];
     for (var i = 0; i < stocks.length; i++) {
       var name = stocks[i].name;
       if (name[name.length - 2] === ' ') {
@@ -56,21 +57,23 @@ function loadBloomberg() {
         name = stocks[i].name.substring(0, stocks[i].name.lastIndexOf(' '));
       }
       myStocks[name] = stocks[i];
-      $('p').highlight(name, {
-        caseSensitive: true,
-        className: 'bloomberg'
-      });
+      names.push(name);
     }
+    $('p').highlight(names, {
+      caseSensitive: true,
+      className: 'bloomberg'
+    });
 
     $('.bloomberg').each(function() {
       var $this = $(this);
       $this.popover({
-        animation:true, 
-        content:"yes", 
-        html: true, 
-        placement:"top", 
-        trigger:"hover", 
-        title:"bitch pls"})
+        animation: true,
+        content: "yes",
+        html: true,
+        placement: "top",
+        trigger: "hover",
+        title: "bitch pls"
+      })
       var stock = myStocks[$this.html()];
     });
   });
@@ -79,24 +82,22 @@ function loadBloomberg() {
 var aapl = null;
 
 function fake(data) {
-        console.log(data)
-	// Create the chart
-        $('#chart-aapl').highcharts('StockChart', {
-          rangeSelector: {
-            selected: 1,
-            inputEnabled: $('#chart-aapl').width() > 480
-          },
-          title: {
-            text: 'AAPL Stock Price'
-          },
-          series: [{
-            name: 'AAPL',
-            data: data,
-            tooltip: {
-              valueDecimals: 2
-            }
-          }]
-        });
+  console.log(data)
+  // Create the chart
+  $('#chart-aapl').highcharts('StockChart', {
+    rangeSelector: {
+      selected: 1,
+      inputEnabled: $('#chart-aapl').width() > 480
+    },
+    title: {
+      text: 'AAPL Stock Price'
+    },
+    series: [{
+      name: 'AAPL',
+      data: data,
+      tooltip: {
+        valueDecimals: 2
+      }
+    }]
+  });
 }
-
-
