8cb499efa3d0030ce87c79d3b5506639be97c8a8
Ian Macalinao <ian@giza.us>
2014-09-06 14:21:55 -0500

    Use join

--- chrome/ext.js
+++ chrome/ext.js
@@ -6,10 +6,7 @@ function loadBloomberg() {
       if (name[name.length - 2] === ' ') {
         name = name.substring(0, name.length - 2);
       }
-      if (name[name.length - 1] === '.' 
-        || name.lastIndexOf('Co') === name.length - 2
-        || name.lastIndexOf('Corp') === name.length - 4
-        || name.lastIndexOf('Inc') === name.length - 3) {
+      if (name[name.length - 1] === '.' || name.lastIndexOf('Co') === name.length - 2 || name.lastIndexOf('Corp') === name.length - 4 || name.lastIndexOf('Inc') === name.length - 3) {
         name = stocks[i].name.substring(0, stocks[i].name.lastIndexOf(' '));
       }
       myStocks[name] = stocks[i];
@@ -23,32 +20,33 @@ function loadBloomberg() {
       var $this = $(this);
       var stock = myStocks[$this.html()];
       $this.qtip({
-        content: '<h3>' + stock.name + '</h3> (NYSE: ' + stock.symbol + ')'
-                   + '<hr />'
-                   + '<div id="chart-' + stock.symbol.toLowerCase() + '" class="stock-chart"></div>'
-                   + '<hr />'
-                   + '<small>Powered by Bloomberg</small>'
+        content: ['<h3>' + stock.name + '</h3> (NYSE: ' + stock.symbol + ')',
+          '<hr />',
+          '<div id="chart-' + stock.symbol.toLowerCase() + '" class="stock-chart"></div>',
+          '<hr />',
+          '<small>Powered by Bloomberg</small>'
+        ].join('');
       });
 
-    $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function (data) {
+      $.getJSON('http://www.highcharts.com/samples/data/jsonp.php?filename=aapl-c.json&callback=?', function(data) {
         // Create the chart
         $('#chart-aapl').highcharts('StockChart', {
-            rangeSelector : {
-                selected : 1,
-                inputEnabled: $('#chart-aapl').width() > 480
-            },
-            title : {
-                text : 'AAPL Stock Price'
-            },
-            series : [{
-                name : 'AAPL',
-                data : data,
-                tooltip: {
-                    valueDecimals: 2
-                }
-            }]
+          rangeSelector: {
+            selected: 1,
+            inputEnabled: $('#chart-aapl').width() > 480
+          },
+          title: {
+            text: 'AAPL Stock Price'
+          },
+          series: [{
+            name: 'AAPL',
+            data: data,
+            tooltip: {
+              valueDecimals: 2
+            }
+          }]
         });
-    });
+      });
     });
   });
 }
