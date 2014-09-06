5fbe588e5614565199c126a0bb81f7610dc3bdab
Ian Macalinao <ian@giza.us>
2014-09-06 14:05:21 -0500

    Add more extensions

--- chrome/ext.js
+++ chrome/ext.js
@@ -3,7 +3,10 @@ function loadBloomberg() {
   $(function() {
     for (var i = 0; i < stocks.length; i++) {
       var name = stocks[i].name;
-      if (name[name.length - 1] === '.' || name.lastIndexOf('Co') === name.length - 2) {
+      if (name[name.length - 1] === '.' 
+        || name.lastIndexOf('Co') === name.length - 2
+        || name.lastIndexOf('Corp') === name.length - 4
+        || name.lastIndexOf('Inc') === name.length - 3) {
         name = stocks[i].name.substring(0, stocks[i].name.lastIndexOf(' '));
       }
       names[name] = stocks[i].name;
