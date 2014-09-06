57c29116b12ee3fa0226651ccb0167be848d2c5a
Nitesh <niteshvgupta@gmail.com>
2014-09-06 14:59:50 -0400

    Changed some styling, fixed syntax error

--- chrome/ext.js
+++ chrome/ext.js
@@ -4,7 +4,7 @@ function loadBloomberg() {
     for (var i = 0; i < stocks.length; i++) {
       var name = stocks[i].name.substring(0, stocks[i].name.lastIndexOf(' '));
       names[name] = stocks[i].name;
-      $('p').highlight(name {
+      $('p').highlight(name, {
         caseSensitive: true,
         className: 'bloomberg'
       });
