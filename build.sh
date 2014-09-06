2cafe6b8085c0ccd43927023285ef0942a3dbbbb
YuXuan Liu <wynd07@gmail.com>
2014-09-06 12:58:53 -0400

    Updated list

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ echo "Creating Build Directory"
 rm -f *.class
 
 echo "Compiling"
-javac -classpath lib/blpapi-3.8.5.1.jar *.java
+javac -classpath lib/blpapi-3.8.5.1.jar:lib/gson-2.2.4.jar *.java
 
 echo "Done"
