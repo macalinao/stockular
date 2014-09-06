f1fa60fe4439621c0404728daddc321ced56d1e8
Ian Macalinao <ian@giza.us>
2014-09-06 13:13:12 -0500

    Highlight stuff with the bloomberg class

---
+++ chrome/ext.js
@@ -0,0 +1,17 @@
+function loadBloomberg() {
+  $(function() {
+    for (var i = 0; i < stocks.length; i++) {
+      $('p').highlight(stocks[i].name, {
+        className: 'bloomberg'
+      });
+      $('p').highlight(stocks[i].symbol, {
+        className: 'bloomberg'
+      });
+    }
+
+    $('.bloomberg').each(function() {
+      $(this).qtip();
+    });
+  });
+}
+loadBloomberg();
