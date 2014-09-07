0a7a11ca303123d831c2e380714252a58be2ddad
Ian Macalinao <ian@giza.us>
2014-09-06 23:26:15 -0500

    Add rift view

--- chrome/ext.js
+++ chrome/ext.js
@@ -129,7 +129,7 @@ function loadBloomberg() {
         placement: "bottom",
         trigger: "none",
         title: '<div class="btn-group"><button type="button" class="btn btn-default">Overview</button>'+
-        '<button type="button" class="btn btn-default">Live View</button><button type="button" class="btn btn-default">Rift View</button></div>' 
+        '<button type="button" class="btn btn-default">Live View</button><button type="button" class="btn btn-default" class="rift-view">Rift View</button></div>' 
       });
 
       abc.titleHtml =         '<b>'+stock.symbol + ' '+close +'</b>' + 
@@ -142,6 +142,10 @@ function loadBloomberg() {
       abc.gdata = gdata;
 
     });
+  });
+
+  $('.rift-view').click(function() {
+    chrome.tabs.create({url: 'rift.html'});
   });
 }
 
