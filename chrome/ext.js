b3d6baf50c425e5dcb9813c18cf10203e450d118
Ian Macalinao <ian@giza.us>
2014-09-06 20:44:06 -0500

    Use content property

--- chrome/ext.js
+++ chrome/ext.js
@@ -91,7 +91,7 @@ function loadBloomberg() {
         var leftData = $('<div class="col"></div>');
         leftData.append('<strong>Market cap:</strong> ' + data.values.CUR_MKT_CAP);
         leftData.append('<strong>P/E Ratio:</strong> ' + data.values.PE_RATIO);
-        popover.content(leftData.html());
+        popover.content = leftData.html();
       });
 
     });
